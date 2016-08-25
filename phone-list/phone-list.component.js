11ddccfa32380314fc6294f972320ba40251db5c
Dhivyakrishnan257 <dhivya.k1995@yahoo.com>
2016-08-25 16:29:42 +0530

    Add files via upload

--- phone-list/phone-list.component.js
+++ phone-list/phone-list.component.js
@@ -9,17 +9,35 @@ angular.
       var self = this;
       self.orderProp = 'year';
       self.currentPage = 0;
-     self.pageSize=4;
+      self.pageSize=4;
+      self.ascsort=true;
       $http.get('phones/crimes.json').then(function(response) {
-        self.phones = response.data;
-        self.pageCount=Math.ceil(self.phones.length/self.pageSize);
-    // self.removeItem = function removeItem(phone) {
-      //   var index =self.phones.indexOf(phone);
-        // if (index !== -1) {
-          //   self.phones.splice(index, 1);
-    //}
-  //};
+      self.phones = response.data;
+     self.pageCount=Math.ceil(self.phones.length/self.pageSize);
 
+       self.removeItem = function removeItem(phone) {
+       var index =self.phones.indexOf(phone);
+       if (index !== -1) {
+           self.phones.splice(index, 1);
+           self.pageCount=Math.ceil(self.phones.length/self.pageSize);
+  }
+  };
+  self.addItem=function addItem()
+{
+  self.phones.push({'year':self.year,'above_500':self.above_500,'under_500':self.under_500});
+  self.pageCount=Math.ceil(self.phones.length/self.pageSize);
+
+  //self.year='';
+
+//  self.above_500='';
+//  self.under_500='';
+}
+self.filter=function filter(option,query)
+{
+  if(self.phones.year==option && self.phones.year==query){
+
+  }
+}
     });
   }
 });
